3e0bdc4b4cfe83a4105a191bcc6f0ad5f473e3d5
sakthivel1 <46038424+sakthivel1@users.noreply.github.com>
2026-03-18 23:46:04 +0530

    Reorder properties in Jest configuration

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,8 +3,8 @@ const nextJest = require('next/jest')
 const createJestConfig = nextJest({ dir: './' })
 
 const customJestConfig = {
-  setupFilesAfterFramework: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jest-environment-jsdom'
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterFramework: ['<rootDir>/jest.setup.js']
 }
 
 module.exports = createJestConfig(customJestConfig)
